cb96ffe9af1486c93e094f6ccdff52d9cff79469
alexsandr <drevs.aleksandr@gmail.com>
2021-01-30 18:25:44 +0600

    снова правки

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно
 const getRandomNumber =  (min, max) => {
   const result = Math.floor(Math.random() * (max - min + 1)) + min;
-  Math.round(min);
-  Math.round(max);
-  if (min >= max) {
+  const minRounded = Math.round(min);
+  const maxRounded = Math.round(max);
+  if (minRounded >= maxRounded) {
     return false;
   }
 
-  if (min >= 0 && max > 0) {
+  if (minRounded >= 0 && maxRounded > 0) {
     return result;
   }
   return false;

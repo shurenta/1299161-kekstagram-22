1971a1a7dd7bd3d1817aec92d978bcc4fddd7d1f
alexsandr <drevs.aleksandr@gmail.com>
2021-01-30 18:05:38 +0600

    сделал правки

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно
 const getRandomNumber =  (min, max) => {
   const result = Math.floor(Math.random() * (max - min + 1)) + min;
-  min = Math.ceil(min);
-  max = Math.floor(max);
+  Math.round(min);
+  Math.round(max);
   if (min >= max) {
-    return alert ('Первое число не может быть больше или равно второму');
+    return false;
   }
 
   if (min >= 0 && max > 0) {
     return result;
-  } return alert ('Число должно быть положительным или 0');
+  }
+  return false;
 }
 getRandomNumber(0, 500);
 
@@ -17,7 +18,6 @@ getRandomNumber(0, 500);
 
 // Функция для проверки максимальной длины строки
 const checkLengthStrings = (checkedString,maximumlength = 140) => {
-  checkedString.length;
-  return (checkedString.length <= maximumlength) ? true : false;
+  return checkedString.length <= maximumlength;
 }
 checkLengthStrings('');

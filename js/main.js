bb0a9199c388c5aa584f6baaa7f1acbbe165769f
alexsandr <drevs.aleksandr@gmail.com>
2021-02-08 12:02:25 +0600

    Исравил замечания

--- js/main.js
+++ js/main.js
@@ -30,8 +30,23 @@ const PHOTO_DESCRIPTION = [
   'Как вам мой новый костюм',
 ];
 
-// Функция, возвращающая случайное целое число из переданного диапазона включительно
+const MIN_NUMBER_ID_PHOTO = 1;
+const MAX_NUMBER_ID_PHOTO = 25;
+const MIN_NUMBER_AVATAR = 1;
+const MAX_NUMBER_AVATAR = 6;
+const MIN_ID_COMMENTS = 1;
+const MAX_ID_COMMENTS = 999;
+const MIN_NUMBER_LIKES = 15;
+const MAX_NUMBER_LIKES = 200;
+const NUMBER_COMMENTS = 10;
 
+let photos = [];
+let comments = [];
+let numbersAndIdPhotos = [];
+let commentsId = [];
+
+
+// Функция, возвращающая случайное целое число из переданного диапазона включительно
 const getRandomNamber =  (min, max) => {
   const result = Math.floor(Math.random() * (max - min + 1)) + min;
   const minRounded = Math.round(min);
@@ -46,51 +61,77 @@ const getRandomNamber =  (min, max) => {
   return false;
 };
 
-// Функция для проверки максимальной длины строки
 
+
+// Функция для проверки максимальной длины строки
 const checkLengthStrings = (checkedString,maximumlength = 140) => {
   return checkedString.length <= maximumlength;
 }
 checkLengthStrings('');
 
-// Функция  возвращающая случайное значение
 
-let getRandom = (array) => {
+
+// Функция  возвращающая случайное значение
+let getRandomArryElement = (array) => {
   let namberRandom = Math.floor(Math.random() * array.length);
   return array[namberRandom];
 }
 
-// Функция генереруящая массив
 
-const createPhotos = () => {
-  const comments = [];
-  const photos = [];
-  const numbers = [];
-  while(numbers.length < 25){
-    const rundom = Math.floor(Math.random() * 25) + 1;
-    if(numbers.indexOf(rundom) === -1) numbers.push(rundom);
+
+// Функция генерируящая id фотографии и номер фото
+let getIdAndNamberPhoto = () => {
+  while(numbersAndIdPhotos.length < MAX_NUMBER_ID_PHOTO) {
+    const rundom = getRandomNamber(MIN_NUMBER_ID_PHOTO, MAX_NUMBER_ID_PHOTO);
+    if(numbersAndIdPhotos.indexOf(rundom) === -1) numbersAndIdPhotos.push(rundom);
   }
+  return numbersAndIdPhotos;
+}
 
-  for (let i = 0; i < numbers.length; i++) {
-    let number = numbers[i];
 
+
+//функция для генерации id комментария
+let getIdComments = () => {
+  while(commentsId.length < MAX_ID_COMMENTS) {
+    const rundom = getRandomNamber(MIN_ID_COMMENTS, MAX_ID_COMMENTS);
+    if(commentsId.indexOf(rundom) === -1) commentsId.push(rundom);
+  }
+  return commentsId;
+}
+
+
+
+// функция создает объекты с коментариями и помещает их в массив
+let getArryCommnets = () => {
+  getIdComments();
+  for (let i = 0; i < NUMBER_COMMENTS; i++) {
     let similarComments = {
-      id: getRandomNamber(0, 9999999999),
-      avatar: 'img/avatar-' + getRandomNamber(1, 6) + '.svg',
-      message: getRandom(COMMENTS),
-      name: getRandom(NAMES),
-    };
+      Id: commentsId[i],
+      avatar: 'img/avatar-' + getRandomNamber(MIN_NUMBER_AVATAR, MAX_NUMBER_AVATAR) + '.svg',
+      message: getRandomArryElement(COMMENTS),
+      name: getRandomArryElement(NAMES),
+    }
     comments.push(similarComments);
+  }
+  return comments;
+}
 
+
+
+// функция создает объекты с фото и помещает их в массив
+let getArryPhoto = () => {
+  getIdAndNamberPhoto();
+  getArryCommnets();
+  for (let i = 0; i < MAX_NUMBER_ID_PHOTO; i++) {
     let similarPhotoPublication = {
-      Id: number,
-      url: 'photos/' + number + '.jpg',
-      description: getRandom(PHOTO_DESCRIPTION),
-      likes: getRandomNamber(15, 200),
+      id: numbersAndIdPhotos[i],
+      url: 'photos/' + numbersAndIdPhotos[i] + '.jpg',
+      description: getRandomArryElement(PHOTO_DESCRIPTION),
+      likes: getRandomNamber(MIN_NUMBER_LIKES, MAX_NUMBER_LIKES),
       comments: comments,
     }
     photos.push(similarPhotoPublication);
   }
   return photos;
-};
-createPhotos();
+}
+getArryPhoto();

87aea91a63ad0bc9342cc7a663374b0c5d749c9b
alexsandr <drevs.aleksandr@gmail.com>
2021-03-06 19:31:43 +0600

    сборка и загрузска фото

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+module.exports = {
+  entry: './source/js/main.js',
+  devtool: 'source-map',
+  output: {
+    filename: 'main.bundle.js',
+    path: path.resolve(__dirname, 'build/js'),
+  }
+};
